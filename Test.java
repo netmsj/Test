e6216f2655d9780cf1acc171fb508dbdb5d8ad3d
netmsj <net_msj@qq.com>
2016-09-20 13:41:40 +0800

    新做修改

--- Test.java
+++ Test.java
@@ -2,5 +2,9 @@ public class Test {
 	public static void main(String args[]) {
 		System.out.println("Hello GIT1 !!!") ;
 		System.out.println("Hello GIT3  ´íÎó !!!") ;
+		System.out.println("Hello GIT3  ´íÎó !!!") ;
+		System.out.println("Hello GIT3  ´íÎó !!!") ;
+		System.out.println("Hello GIT3  ´íÎó4 !!!") ;
+
 	}
 }
